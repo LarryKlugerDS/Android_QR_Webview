aeac0bf9b8838570f9c7f22485aa38b439c54e3f
Larry Kluger <larry.kluger@docusign.com>
2024-05-26 16:07:37 +0300

    Working but no postSign
    The signing ceremony opens, but the PostMsg is not seen in JavaScript-land

--- sample/src/main/kotlin/io/github/g00fy2/quickiesample/WebViewActivity.kt
+++ sample/src/main/kotlin/io/github/g00fy2/quickiesample/WebViewActivity.kt
@@ -1,23 +1,35 @@
 package io.github.g00fy2.quickiesample
 import android.annotation.SuppressLint
-import android.icu.text.DisplayContext
+import android.content.Intent
 import android.os.Bundle
-import android.view.WindowInsets
 import android.webkit.JavascriptInterface
+import android.webkit.WebResourceRequest
 import android.webkit.WebView
+import android.webkit.WebViewClient
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
 
-class WebViewActivity : AppCompatActivity() {
+public class WebViewActivity : AppCompatActivity() {
 
-  public class JsObject {
-    @JavascriptInterface
-    public fun msg(): String {
-      return "From Android!"
+  class MyWebViewClient : WebViewClient() {
+    override fun shouldOverrideUrlLoading(view: WebView, request: WebResourceRequest): Boolean {
+      return false
     }
+  }
+
+  @JavascriptInterface
+  public fun msg(): String {
+    return "From Android!"
+  }
+
+  @JavascriptInterface
+  public fun postMsg(msg: String) {
+    val resultActivityIntent = Intent(this, ResultActivity::class.java)
+    resultActivityIntent.putExtra("postMsg", msg);
+    startActivity(resultActivityIntent)
   }
 
   override fun onCreate(savedInstanceState: Bundle?) {
@@ -38,15 +50,35 @@ class WebViewActivity : AppCompatActivity() {
         supportActionBar?.hide()
 
         val bundle = intent.extras
-        val url = bundle?.getString("url")
+        val url = bundle!!.getString("url")
 
         val myWebView: WebView = findViewById(R.id.webview)
-        @SuppressLint("SetJavaScriptEnabled")
-        myWebView.getSettings().javaScriptEnabled = true
-        myWebView.addJavascriptInterface(JsObject(), "Android")
+        myWebView.webViewClient = WebViewClient()
 
-        myWebView.loadUrl("https://docusign.github.io/examples/androidTest.html?$url")
+        val wvSettings = myWebView.getSettings()
+        @SuppressLint("SetJavaScriptEnabled")
+        wvSettings.javaScriptEnabled = true
+        wvSettings.domStorageEnabled = true
+        //wvSettings.loadWithOverviewMode = true
+        //wvSettings.useWideViewPort = true
+        //wvSettings.builtInZoomControls = true
+        //wvSettings.displayZoomControls = true
+        //wvSettings.defaultTextEncodingName = "utf-8"
+
+        myWebView.addJavascriptInterface(this, "Android")
+
+        //myWebView.loadUrl("https://docusign.github.io/examples/androidTest.html")
+        myWebView.loadUrl(url!!)
+
+        // REMOVE Uses url to stop lint errors if url is not used above
+        val resultActivityIntent = Intent(this, ResultActivity::class.java)
+        resultActivityIntent.putExtra("foo", url);
+
+
         //myWebView.loadUrl("https://xerox.com?$url")
-        //myWebView.loadUrl(url!!)
   }
+
+
+
+
 }
