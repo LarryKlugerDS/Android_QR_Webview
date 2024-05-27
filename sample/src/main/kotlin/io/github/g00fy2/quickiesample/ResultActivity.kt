1626a4ee283ee2237251d12808f288054fc0ff83
Larry Kluger <larry.kluger@docusign.com>
2024-05-27 23:12:22 +0300

    Fixed results text display

--- sample/src/main/kotlin/io/github/g00fy2/quickiesample/ResultActivity.kt
+++ sample/src/main/kotlin/io/github/g00fy2/quickiesample/ResultActivity.kt
@@ -2,6 +2,9 @@ package io.github.g00fy2.quickiesample
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.view.View
 import android.webkit.JavascriptInterface
 import android.webkit.WebView
 import android.widget.TextView
@@ -11,16 +14,32 @@ import androidx.core.content.ContextCompat.startActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
+import io.github.g00fy2.quickiesample.databinding.ActivityMainBinding
+import io.github.g00fy2.quickiesample.databinding.ActivityResultBinding
 
 public class ResultActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityResultBinding
 
   override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
         val bundle = intent.extras
-        val postMsg = bundle?.getString("postMsg")
+        val postMsg = bundle!!.getString("postMsg")
 
-        val myTextView: TextView = findViewById(R.id.textView)
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val myTextView: TextView = findViewById(R.id.textViewResult)
         myTextView.text = postMsg
+
+        binding.continueToMain.setOnClickListener {
+          continueToMainClick
+        }
+
+  }
+
+  val continueToMainClick = View.OnClickListener {
+    val resultActivityIntent = Intent(this, MainActivity::class.java)
+    startActivity(resultActivityIntent)
   }
 }
